d53a0ea9f2986d8d76b4bef2eb4bd8e6e46cd63b
phumiwit <68888298+phumiwit@users.noreply.github.com>
2022-12-04 23:05:13 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, File, UploadFile
+from tensorflow import keras
+import numpy as np
+import librosa
+import math
+from pydub import AudioSegment
+import math
+import os
+import pickle
+from keyword_spotting_service import Keyword_Spotting_Service
+
+app = FastAPI()
+# pickle_in = open("Music_genre_CNN.pkl","rb")
+# CNN = pickle.load(pickle_in)
+
+
+@app.get('/')
+def predict_genre(path : str):
+    kss = Keyword_Spotting_Service()
+    keyword1,keyword2= kss.prediction(path)
+    return {"prediction":keyword1}
+
+@app.get('/value')
+def value_genre(path : str):
+    kss = Keyword_Spotting_Service()
+    keyword1,keyword2 = kss.prediction(path)
+    
+    return keyword2
